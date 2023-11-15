485ecaf39c18738cf27417e3eaf1ad180d1773f8
kimiko ando <kimiko.ando13@icloud.com>
2023-11-15 12:52:20 +0900

    created the home and booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def create
+    @property = Property.find(params[:property_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.property = @property
+    if @booking.save
+      flash[:notice] = "Booking created!"
+      redirect_to property_path(@property)
+    else
+      render "properties/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end

b8713b874a35897c935b5645a584580f931409fc
Nick Lin <nk.shao0204@gmail.com>
2023-11-15 10:44:18 +0900

    put some pictures

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,9 @@
 class PropertiesController < ApplicationController
+  def index
+    if params[:category]
+      @properties = Property.select{|property| property.category ==  params[:category]}
+    else
+      @properties = Property.all
+    end
+  end
 end
